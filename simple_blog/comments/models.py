7075bbc13cfd4ee3693c08e945956a386fffdceb
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-06-02 14:11:37 -0400

    adding models

---
+++ simple_blog/comments/models.py
@@ -0,0 +1,23 @@
+import profile
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+from posts.models import Post
+
+# Model
+
+# Create your models here.
+class Comment(models.Model):
+    """Comment model."""
+
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    profile = models.models.ForeignKey('users.profile', on_delete=models.PROTECT )
+    post = models.ForeignKey(Post, on_delete=models.PROTECT)
+    comment = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.comment
+    
+    
